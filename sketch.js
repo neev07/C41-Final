9fc2be11472b6824e12bc697d3c886733a327a40
neev07 <64964514+neev07@users.noreply.github.com>
2020-07-13 11:34:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup() {
 function draw() {
   background(230, 230, 250);
   for (var i = 0; i < drops.length; i++) {
-    drops[i].fall();
+//     drops[i].fall();
     drops[i].show();
   }
   drawSprites();
