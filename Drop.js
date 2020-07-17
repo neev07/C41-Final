6dbc6913ac220b094357ce8ff5ead48d9a4dd781
neev07 <64964514+neev07@users.noreply.github.com>
2020-07-17 13:42:57 +0530

    Update Drop.js

--- Drop.js
+++ Drop.js
@@ -1,13 +1,13 @@
-class Drop {
-    constructor(x,y,z,len,yspeed){
+function Drop {
+  
     this.x = random(width);
     this.y = random(-500, -50);
     this.z = random(0, 20);
     this.len = map(this.z, 0, 20, 10, 20);
     this.yspeed = map(this.z, 0, 20, 1, 20);
-    }
   
-    fall() {
+  
+    this.fall = function() {
       this.y = this.y + this.yspeed;
       var grav = map(this.z, 0, 20, 0, 0.2);
       this.yspeed = this.yspeed + grav;
@@ -18,7 +18,7 @@ class Drop {
       }
     };
   
-    show() {
+    this.show = function() {
       var thick = map(this.z, 0, 20, 1, 3);
       strokeWeight(thick);
       stroke(138, 43, 226);
